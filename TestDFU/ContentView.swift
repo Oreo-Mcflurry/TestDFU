e8e84b446cc038ae40698c83c534b167dd11eff9
유인호 <yuiop1029@naver.com>
2024-12-11 09:11:34 +0900

    Initial Commit

---
+++ TestDFU/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestDFU
+//
+//  Created by A_Mcflurry on 12/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestDFU/TestDFUApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestDFUApp.swift
+//  TestDFU
+//
+//  Created by A_Mcflurry on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TestDFUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
